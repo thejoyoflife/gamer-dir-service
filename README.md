d1f3a16724d787b0f152044be1d38bc0009e03b0
Shimul Kumar Saha <thejoyoflife@gmail.com>
2024-01-23 22:08:16 +0100

    chore: initialize an empty spring boot project
    Initialized the project using spring initializer and added the
    required dependencies and essential properties.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gamer Directory Service
+
+A service to manage directory of games, gamers and their credits to individual games.
+
+### Deployment Instruction
+The easier way to deploy the application is to download or clone the repository and run '`docker compose up -d`' inside the un-zipped or cloned directory.
+
+
---
+++ src/main/java/com/example/gamer/directory/GamerDirectoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamer.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerDirectoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerDirectoryServiceApplication.class, args);
+	}
+
+}
