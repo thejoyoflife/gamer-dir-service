e39183197e819128e15310576ac8d41beafb6be9
Shimul Kumar Saha <thejoyoflife@gmail.com>
2024-01-24 23:06:37 +0100

    feat(credit): max-credits api response format is made obvious
    Earlier a Map was being used for conveying the gamer information.
    Now, a Class is being used to represent the gamer. Now, the Swagger UI
    shows the format more explicitly. Also, the Swagger documenation for
    this response model has been added.

--- src/main/java/com/example/gamer/directory/domain/dtos/MaxCreditGameDTO.java
+++ src/main/java/com/example/gamer/directory/domain/dtos/MaxCreditGameDTO.java
@@ -9,33 +9,50 @@ import static com.example.gamer.directory.repositories.extras.MaxCreditQueryCons
 import static com.example.gamer.directory.utils.Utils.toInt;
 import static com.example.gamer.directory.utils.Utils.toLong;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class MaxCreditGameDTO {
+	@Schema(description = "ID of the Game", example = "1")
 	private Integer gameId;
+	
+	@Schema(description = "Name of the Game", example = "fortnite")
 	private String gameName;
+	
+	@Schema(description = "Level of interest shown in the game", example = "noob")
 	private String level;
 	
-	private TopScorer topScore = new TopScorer();
+	@Schema(description = "Gamer with max credit or top scorer")
+	private TopScore topScore = new TopScore();
 	
 	public MaxCreditGameDTO(Object[] tuple, Map<String, Integer> aliasToIndexMap) {
 		this.gameId = toInt(tuple[aliasToIndexMap.get(GAME_ID)]);
 		this.gameName = String.valueOf(tuple[aliasToIndexMap.get(GAME_NAME)]);
 		this.level = String.valueOf(tuple[aliasToIndexMap.get(INTEREST_LEVEL)]);
 		
-		topScore.getGamer().put("id", toLong(tuple[aliasToIndexMap.get(GAMER_ID)]));
-		topScore.getGamer().put("name", String.valueOf(tuple[aliasToIndexMap.get(GAMER_NAME)]));
+		topScore.getGamer().setId(toLong(tuple[aliasToIndexMap.get(GAMER_ID)]));
+		topScore.getGamer().setName(String.valueOf(tuple[aliasToIndexMap.get(GAMER_NAME)]));
 		
 		topScore.setCredit(toInt(tuple[aliasToIndexMap.get(CREDIT)]));
 	}
 	
 	@Data
-	public class TopScorer {
-		private Map<String, Object> gamer = new HashMap<>();
+	static class TopScore {
+		@Data
+		static class TopScorer {
+			@Schema(description = "ID of the Gamer", example = "1")
+			private Long id;
+	
+			@Schema(description = "Name of the Gamer", example = "XXX YYYY")
+			private String name;
+		}
+		@Schema(description = "Top Scorer")
+		private TopScorer gamer = new TopScorer();
+		
+		@Schema(description = "Credit awarded to him for the game", example = "10")
 		private Integer credit;
 	}
 }
